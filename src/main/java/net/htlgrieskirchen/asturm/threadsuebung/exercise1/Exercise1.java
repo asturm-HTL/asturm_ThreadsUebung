236cf0d0e9c74f3d16c4f618cf8c34300506b69f
Andreas Sturm <Andi_Sturm@gmx.at>
2019-10-09 14:21:10 +0200

    first variables and methods

--- src/main/java/net/htlgrieskirchen/asturm/threadsuebung/exercise1/Exercise1.java
+++ src/main/java/net/htlgrieskirchen/asturm/threadsuebung/exercise1/Exercise1.java
@@ -5,10 +5,55 @@
  */
 package net.htlgrieskirchen.asturm.threadsuebung.exercise1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author user
  */
-public class Exercise1 {
+public class Exercise1 
+{
     
+    List<Integer> arrayList = new ArrayList<>();
+    static String fileName = "C:\\Users\\user\\Documents\\NetBeansProjects\\asturm_ThreadsUebung\\numbers.csv";
+    
+    public static void main(String[] args) throws IOException 
+    {
+    
+        try 
+        {
+            
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            
+            while(br.readLine() != null)
+            {
+                
+                try
+                {
+                    
+                    
+                    
+                }
+                catch(Exception exc)
+                {
+                    
+                }
+                
+                        
+            }
+        
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            Logger.getLogger(Exercise1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
