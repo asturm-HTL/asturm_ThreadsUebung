7593d7f94aed8c8f33428ebe121033c189a16c39
Andreas Sturm <Andi_Sturm@gmx.at>
2019-10-09 15:11:14 +0200

    Implementing Divider Logic

--- src/main/java/net/htlgrieskirchen/asturm/threadsuebung/exercise1/Exercise1.java
+++ src/main/java/net/htlgrieskirchen/asturm/threadsuebung/exercise1/Exercise1.java
@@ -11,6 +11,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -50,13 +51,21 @@ public class Exercise1
                         }
                         catch(Exception e)
                         {
-                            System.out.println("Eintrag war keine Zahl oder ein Buchstabe war vorhanden");
+                            //System.out.println("Eintrag war keine Zahl oder ein Buchstabe war vorhanden");
                         }
                         
                     }
 
-                    System.out.println(arrayListInteger);
+                    //System.out.println(arrayListInteger);
                 }
+                
+                System.out.println("----------------------------------------------------");
+                System.out.println("Geben Sie bitte einen Teiler ein :");
+                
+                    Scanner input = new Scanner(System.in);
+                
+                int test = input.nextInt();
+                
             
             
         } 
